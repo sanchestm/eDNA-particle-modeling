a3ae1765ba0754dfd0a12dc40c71f3ab78ed74f2
sanchesTM <thiago.missfeldt@gmail.com>
2022-03-16 17:44:52 -0400

    minor changes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 
 setuptools.setup(
-    name='eDNA-particle-modeling',
+    name='eDNAtransport',
     version='0.0.1',
     author='Thiago Sanches',
     author_email='none',
     description='Testing installation of Package',
     url='https://github.com/sanchestm/eDNA-particle-modeling/',
     license='MIT',
-    packages=['eDNA-particle-modeling'],
+    packages=['eDNAtransport'],
     install_requires=['numpy','pymc3>=3.8','arviz','scipy','ipywidgets','scikit-learn','seaborn','matplotlib','pandas','statsmodels'],
 )
